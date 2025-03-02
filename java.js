8f6e75b0db7a650544336879746ebfcc3f5afa7d
tajcreator <mdmuhibbullah397@gmail.com>
2025-03-02 20:08:51 +0600

    box-model

--- java.js
+++ java.js
@@ -20,4 +20,31 @@ document.addEventListener("click", () => {
   });
 
 
-  
+ document.getElementById('change-page').addEventListener('click', function() {
+    {
+        window.location = '/different.side/newPage.html';
+      }
+ }) 
+
+  let day = document.querySelector('#day');
+let fullDate = document.querySelector('#fullDate');
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const date = new Date();
+day.innerHTML = `${days[date.getDay()]},`;
+fullDate.innerHTML = `${months[date.getMonth()]} ${
+  date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+} ${date.getFullYear()}`;
